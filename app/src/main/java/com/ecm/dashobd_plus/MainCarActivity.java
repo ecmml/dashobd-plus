80847e7850c7a344552750bc0a8c2c4dc75685e3
molotfi07 <140925601+molotfi07@users.noreply.github.com>
2023-10-03 17:49:00 -0700

    Initlial setup

--- app/src/main/java/com/ecm/dashobd_plus/MainCarActivity.java
+++ app/src/main/java/com/ecm/dashobd_plus/MainCarActivity.java
@@ -1,10 +1,17 @@
 package com.ecm.dashobd_plus;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
 
 import android.os.Bundle;
 
 import com.google.android.apps.auto.sdk.CarActivity;
+import com.google.android.apps.auto.sdk.MenuAdapter;
+import com.google.android.apps.auto.sdk.MenuAdapterCallback;
+import com.google.android.apps.auto.sdk.MenuController;
+import com.google.android.apps.auto.sdk.MenuItem;
+import com.google.android.apps.auto.sdk.m;
 
 public class MainCarActivity extends CarActivity {
 
@@ -12,5 +19,28 @@ public class MainCarActivity extends CarActivity {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_car);
+
+
+        FragmentManager fm = getSupportFragmentManager();
+        Fragment currentFragment;
+        HomeFragment homeFragment = new HomeFragment();
+
+        fm.beginTransaction()
+                .add(R.id.fragment_container, homeFragment, HomeFragment.class.getSimpleName())
+                .hide(homeFragment).commit();
+
+
+        fm.beginTransaction().show(homeFragment).commit();
+        
+
+
+
+
+
+
+
+
+
+
     }
 }
